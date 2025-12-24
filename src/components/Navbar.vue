a808de9a94489ba92857c5164faffdbd1f5bfdb1
chickundersea <chickundersea@gmail.com>
2025-12-24 11:22:16 +0800

    新增: Navbar 切塊

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,10 @@
 <template>
-
-
-    
+  <header class="relative bg-red-50">
+    <nav class="fixed top-0 z-1">
+      navbar 佔位區
+      <div class="logo"></div>
+      <div class="menu"></div>
+      <div class="member"></div>
+    </nav>
+  </header>    
 </template>
