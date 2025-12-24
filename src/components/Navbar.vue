4445124f63418c28ae6d27eea7b2630e84db0a66
chickundersea <chickundersea@gmail.com>
2025-12-25 02:51:57 +0800

    新增: Navbar 會員登入與註冊按鈕

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,8 +3,16 @@
     <nav class="fixed top-0 z-1">
       navbar 佔位區
       <div class="logo"></div>
-      <div class="menu"></div>
-      <div class="member"></div>
+      <div class="login">
+        <button>
+          會員登入
+        </button>
+      </div>
+      <div class="register">
+        <button>
+          加入會員
+        </button>
+      </div>
     </nav>
-  </header>    
+  </header>
 </template>
