c890839f3eaaace4666d148a39b3dd22b59d99ea
chickundersea <chickundersea@gmail.com>
2025-12-28 06:32:30 +0800

    更新: 調整 Footer 背景顏色及版權年份

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,10 @@
 
 </script>
 <template>
-  <footer class="bg-gray-800 text-white py-6 mt-10">
+  <footer class="bg-stone-500 text-white py-10 mt-10">
     <div class="max-w-[1440px] mx-auto px-[5%] sm:px-[8%] lg:px-[5%] text-center">
-      <p class="text-sm sm:text-base">&copy; 2024 carE. All rights reserved.</p>
+      <p class="text-sm sm:text-base">&copy; 2025 carE. All rights reserved.</p>
+
     </div>
   </footer>
 </template>
