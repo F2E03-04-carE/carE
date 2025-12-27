79dc77a42fec24a1d45941eec9921473398d5633
chickundersea <chickundersea@gmail.com>
2025-12-28 06:00:41 +0800

    新增: 在 HeaderNavbar 中加入登入與註冊按鈕

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import HeaderNavbar from './components/HeaderNavbar.vue';
 import Home from './components/Home.vue'
 </script>
 
 <template>
+  <HeaderNavbar />
   <Home />
 </template>
 
