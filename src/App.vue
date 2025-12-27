2083cfd58e8524411c42c70c2c4eef3163f5793e
chickundersea <chickundersea@gmail.com>
2025-12-28 06:20:06 +0800

    新增: Footer 組件並在 App 中引入，移除 Navbar 組件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeaderNavbar from './components/HeaderNavbar.vue';
 import Home from './components/Home.vue'
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <HeaderNavbar />
   <Home />
+  <Footer />
 </template>
 
 <style scoped></style>
