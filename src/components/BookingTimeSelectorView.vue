1ac149aee4f7d5e8b298f5f83e4277a6e8eacd23
hoooom6600 <hoooom6600@gmail.com>
2026-01-03 03:12:31 +0800

    新增: 安裝 VCalendar 日期挑選套件並套用

---
+++ src/components/BookingTimeSelectorView.vue
@@ -0,0 +1,16 @@
+<!-- 日期選擇器。
+時段選擇器。
+預約緩衝時間設定 (預設間隔1小時)
+顯示剩餘可接單數
+(預設2位、0位代表該時端不可預約) -->
+<script setup lang="ts">
+import { ref } from 'vue'
+import { DatePicker } from 'v-calendar'
+import 'v-calendar/style.css'
+
+const selectedDate = ref<Date | null>(new Date())
+const today = new Date()
+</script>
+<template>
+  <DatePicker :min-date="today" />
+</template>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import App from './App.vue'
 import router from './router'
 import './main.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import { setupCalendar } from 'v-calendar'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(setupCalendar, {})
 
 app.mount('#app')
