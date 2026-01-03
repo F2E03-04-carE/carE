e3e49087c80c7c237428321a1716eb15939bcd85
hoooom6600 <hoooom6600@gmail.com>
2026-01-04 06:18:14 +0800

    新增: 選日期邏輯、確認日期的 v-if / else 渲染 template

--- src/components/BookingTimeSelectorView.vue
+++ src/components/BookingTimeSelectorView.vue
@@ -4,13 +4,30 @@
 顯示剩餘可接單數
 (預設2位、0位代表該時端不可預約) -->
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { DatePicker } from 'v-calendar'
 import 'v-calendar/style.css'
 
-const selectedDate = ref<Date | null>(new Date())
-const today = new Date()
+const selectedDate = ref<Date | null>(null)
+
+const today: Date = new Date()
+
+function onDateSelected(date: Date | null): void {
+  selectedDate.value = date
+}
+
+const formattedDate = computed<string | null>(() => {
+  if (!selectedDate.value) return null
+
+  return selectedDate.value.toLocaleDateString('zh-TW', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+  })
+})
 </script>
+
 <template>
   <div class="bg-[#faf8f5] p-12">
     <div
@@ -21,15 +38,23 @@ const today = new Date()
       <h2 class="mb-3 text-[20px] text-[#3a3a38] font-bold">選擇日期</h2>
       <section class="flex flex-wrap justify-between">
         <div class="w-full md:w-[40%]">
-          <!-- <DatePicker v-model="selectedDate" :min-date="today" class="" /> -->
-          <div class="border border-[#e9e9e7] rounded-[8px]">日期選取器佔位</div>
+          <DatePicker
+            v-model="selectedDate"
+            @update:model-value="onDateSelected"
+            :min-date="today"
+            class="w-full border border-[#e9e9e7] rounded-[8px]"
+          />
         </div>
         <div class="flex flex-col w-full md:w-[55%] my-6 md:my-0">
-          <div
-            class="flex flex-col justify-center items-center p-4 bg-[#fbfbfa] border border-[#e9e9e7] rounded-[8px]"
-          >
-            <i class="fa-regular fa-calendar text-[50px] text-[#c2c2c0]"></i>
-            <p class="mt-3 text-[14px] text-[#8a8a86]">請選擇日期</p>
+          <div class="p-4 bg-[#fbfbfa] border border-[#e9e9e7] rounded-[8px]">
+            <div v-if="!formattedDate" class="flex flex-col justify-center items-center">
+              <i class="fa-regular fa-calendar text-[50px] text-[#c2c2c0]"></i>
+              <p class="mt-3 text-[14px] text-[#8a8a86]">請選擇日期</p>
+            </div>
+            <div v-else class="flex flex-row justify-left items-center">
+              <i class="fa-regular fa-calendar text-[24px] text-[#6b6b5c]"></i>
+              <span class="ml-2 text-[18px] text-[#3a3a38] font-bold">{{ formattedDate }}</span>
+            </div>
           </div>
           <div>
             <p class="my-2 text-[14px] text-[#8a8a86] font-black">選擇時段</p>
