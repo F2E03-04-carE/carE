29011fc0a8b212810effdce45cd65253f9d35463
hoooom6600 <hoooom6600@gmail.com>
2026-01-04 21:05:54 +0800

    安裝: Tailwind v4.1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './main.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
 import { setupCalendar } from 'v-calendar'
 
 const app = createApp(App)
