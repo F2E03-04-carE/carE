cede75f625d74222aff2578e02a4fcb475d70046
chickundersea <chickundersea@gmail.com>
2026-01-07 20:48:59 +0800

    新增：完成卡片內樣式layout

--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -31,7 +31,44 @@ const handleViewDetail = () => {
 </script>
 
 <template>
-  <div class="grid ">
-
+  <div class="border border-[#DBCEBD] rounded-[5px] overflow-hidden hover:shadow-md duration-300 bg-white">
+    <img
+      :src="shop.image || 'https://picsum.photos/300/200?random=1'"
+      :alt="`${shop.name}環境圖片`"
+      class="w-full aspect-[3/2] object-cover"
+    />
+    <div class="px-5 py-3 text-[#4a4a43]">
+      <p class="my-1 font-semibold">{{ shop.name }}</p>
+      <p class="my-1">
+        <span v-html="getStars(shop.score)"></span>
+      </p>
+        <p class="flex flex-row justify-start items-center my-1">
+        <span class="material-symbols-outlined">location_on</span>
+        {{ shop.distance }} 公里
+      </p>
+      <div class="flex flex-row flex-wrap justify-start items-center gap-2 my-5">
+        <!-- 用Tag元件顯示 -->
+        <Tag
+          v-for="(brand, index) in shop.brands"
+          :key="`brand-${index}`"
+          :label="brand"
+          variant="filled"
+        />
+      </div>
+      <div class="flex flex-row flex-wrap justify-start items-center gap-2 my-5">
+        <Tag
+          v-for="(service, index) in shop.services"
+          :key="`service-${index}`"
+          :label="service"
+          variant="outlined"
+        />
+      </div>
+      <button
+        @click="handleViewDetail"
+        class="w-full block p-3 bg-[#8b7d6b] text-[14px] text-white rounded-[8px] cursor-pointer hover:bg-[#6d6250] transition-colors"
+      >
+        查看詳細資料
+      </button>
+    </div>
   </div>
 </template>
