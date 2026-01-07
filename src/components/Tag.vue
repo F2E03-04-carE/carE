1ac2597aef5fa9fbebd0e0fabab102f658c2702b
chickundersea <chickundersea@gmail.com>
2026-01-07 19:06:00 +0800

    新增：Tag元件

---
+++ src/components/Tag.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+interface CategoryTagProps {
+  variant?: 'filled' | 'outlined';
+  label: string;
+}
+
+const props = withDefaults(defineProps<CategoryTagProps>(),{
+  variant: 'outlined',
+});
+
+
+</script>
+<template>
+
+</template>
