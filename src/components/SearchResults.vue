cc3933ac327234e3ac680d3d21737ea46ceabc2f
chickundersea <chickundersea@gmail.com>
2026-01-08 05:29:14 +0800

    新增：篩選與排序交互條件

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -100,6 +100,7 @@ const handleViewDetail = (shopId: number) => {
   // TODO: 之後這裡會接路由跳轉
   alert(`查看商店 ID: ${shopId} 的詳細資料`);
 };
+
 onMounted(() => {
   fetchShops();
 });
@@ -138,14 +139,38 @@ onMounted(() => {
           </select>
         </div>
       </div>
+      <div class="mt-4 text-[#4a4a43] text-sm" v-if="allShops.length > 0">
+        <p>
+          <span class="font-semibold">顯示：</span>
+            <span v-if="filterBy === 'all'">全部</span>
+            <span v-else-if="filterBy === 'nearby'">附近（3公里內）</span>
+            <span v-else-if="filterBy === 'ratingGood'">評價 4 星以上</span>
+          <span class="mx-2">|</span>
+          <span class="font-semibold">排序：</span>
+            <span v-if="filterBy === 'all'">
+            <span v-if="sortBy === 'rating'">依評價</span>
+            <span v-else-if="sortBy === 'distance'">依距離</span>
+            <span v-else>依評論數</span>
+          </span>
+          <span v-else>
+            <!-- 篩選後 -->
+            <span v-if="sortBy === 'rating'">原始順序</span>
+            <span v-else-if="sortBy === 'distance'">依距離</span>
+            <span v-else>依評論數</span>
+          </span>
+          <span class="ml-2 text-gray-600">(共 {{ resultsCount }} 間)</span>
+        </p>
+      </div>
     </div>
   </section>
+
   <section class="pt-5 bg-[#f5f1ed] pb-10 min-h-[60vh]">
     <div class="container mx-auto">
       <div v-if="!resultsCount" class="text-center py-10">
         <p class="text-[#4a4a43] text-lg">查無相關結果</p>
+        <p class="text-[#4a4a43] text-sm mt-2">請嘗試調整篩選條件</p>
       </div>
-      <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div v-else class="grid grid-cols-1 mx-2 md:grid-cols-2 mx-2 gap-3 lg:grid-cols-3 gap-5">
         <ShopCard
           v-for="shop in results"
           :key="shop.id"
