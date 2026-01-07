88cf6ded376299726eaabe145724a586c3b72abe
chickundersea <chickundersea@gmail.com>
2026-01-07 19:37:52 +0800

    新增：為Tag元件定義tagClass計算屬性

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -9,6 +9,15 @@ const props = withDefaults(defineProps<CategoryTagProps>(),{
   variant: 'outlined',
 });
 
+const tagClass = computed(() => {
+  const baseClass = 'inline-block px-3 py-1 text-[14px] rounded-full';
+
+  if (props.variant === 'filled') {
+    return `${baseClass} bg-[#8b7d6b] text-white`;
+  } else {
+    return `${baseClass} bg-[#f5f1ed] text-[#8b7d6b] border border-[#8b7d6b]`;
+  }
+});
 
 </script>
 <template>
