20567337ec15e6bb42bc815a11a775aef98b364b
hoooom6600 <hoooom6600@gmail.com>
2026-01-08 16:55:05 +0800

    新增: 行事曆畫面，資料暫時寫死

---
+++ src/components/shop/Schedule.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+const calendarOptions = ref({
+  plugins: [dayGridPlugin, interactionPlugin],
+  initialView: 'dayGridMonth',
+  //   events array 要撈資料庫，目前寫死以預覽
+  events: [
+    { title: 'Event 1', date: '2026-01-05' },
+    { title: 'Event 2', date: '2026-01-12' },
+  ],
+})
+</script>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
