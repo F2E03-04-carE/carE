7501bda0c06b599084aa403d544fc4735e17537f
hoooom6600 <hoooom6600@gmail.com>
2026-01-09 16:00:10 +0800

    新增: 維修廠後台 router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,71 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import ShopAdminLayout from '@/components/shop/layout/ShopAdminLayout.vue'
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: ShopAdminLayout,
+      children: [
+        {
+          path: '',
+          redirect: '/overview',
+        },
+        {
+          path: 'overview',
+          name: 'overview',
+          component: () => import('@/components/shop/Overview.vue'),
+        },
+        {
+          path: 'orders',
+          name: 'orders',
+          component: () => import('@/components/shop/Orders.vue'),
+        },
+        {
+          path: 'schedule',
+          name: 'schedule',
+          component: () => import('@/components/shop/Schedule.vue'),
+        },
+        {
+          path: 'records',
+          name: 'records',
+          component: () => import('@/components/shop/Records.vue'),
+        },
+        {
+          path: 'edit',
+          component: () => import('@/components/shop/edit/Edit.vue'),
+          children: [
+            {
+              path: '',
+              redirect: { name: 'info' },
+            },
+            {
+              path: 'info',
+              name: 'info',
+              component: () => import('@/components/shop/edit/components/Info.vue'),
+            },
+            {
+              path: 'hours',
+              name: 'hours',
+              component: () => import('@/components/shop/edit/components/Hours.vue'),
+            },
+            {
+              path: 'photos',
+              name: 'photos',
+              component: () => import('@/components/shop/edit/components/Photos.vue'),
+            },
+          ],
+        },
+        {
+          path: 'billing',
+          name: 'billing',
+          component: () => import('@/components/shop/Billing.vue'),
+        },
+      ],
+    },
+  ],
 })
 
 export default router
