d4b169568eaba9a80eac785f955fa52210f448b8
chickundersea <chickundersea@gmail.com>
2026-01-17 04:26:45 +0800

    新增：更換logo與微調文字

--- frontend/src/components/layout/Footer.vue
+++ frontend/src/components/layout/Footer.vue
@@ -1,27 +1,29 @@
 <script setup lang="ts"></script>
 <template>
-  <footer class="bg-stone-400 text-white py-10 mt-10">
+  <footer class="bg-stone-400 text-white py-20">
     <div class="max-w-[1440px] mx-auto px-[5%] sm:px-[8%] lg:px-[5%]">
       <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8 max-w-5xl mx-auto text-center">
         <div class="text-center">
           <div class="flex items-center gap-2 mb-4 justify-center">
-            <span class="logo material-symbols-outlined text-[#6b6b5a]"> directions_car </span>
-            <span class="text-[24px] font-bold">carE</span>
+            <img
+            src="@/assets/icons/carE_logo_white.png"
+            alt="carE logo"
+            href="/"
+            class="h-15 sm:h-18 w-auto object-contain cursor-pointer"
+             />
           </div>
-          <p class="text-sm">給您的愛車最好的照顧</p>
         </div>
         <div class="text-center">
-          <h3 class="font-bold mb-4">網站地圖</h3>
+          <h3 class="font-bold mb-5 text-[18px]">網站地圖</h3>
           <ul class="space-y-2 text-sm">
-            <li><a href="/" class="hover:text-white transition-colors">尋找車廠</a></li>
-            <li><a href="/" class="hover:text-white transition-colors">愛車交流</a></li>
+            <li><a href="/" class="hover:text-[#e9c667]">尋找維修廠</a></li>
+            <li><a href="/join-garage" class="hover:text-[#e9c667]">刊登維修廠</a></li>
           </ul>
         </div>
         <div class="text-center">
-          <h3 class="font-bold mb-4">聯絡我們</h3>
+          <h3 class="font-bold mb-5 text-[18px]">聯絡我們</h3>
           <ul class="space-y-2 text-sm">
-            <li>Email: info@care.com</li>
-            <li>電話: 02-1234-5678</li>
+            <li>Email: <a href="mailto:info@care.com" class="hover:text-[#e9c667]">info@care.com</a></li>
           </ul>
         </div>
       </div>
