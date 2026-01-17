99c15485caf6c2247c0b834f51b2fd59bcbe9875
hoooom6600 <hoooom6600@gmail.com>
2026-01-18 04:19:35 +0800

    新增: 分頁標題元件與搜尋欄元件

---
+++ frontend/src/views/ShopAdmin/components/PageHead.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  subtitle: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-semibold text-[#4a4a43]">{{ title }}</h1>
+    <p class="mt-1 text-[#8a8a7d]">{{ subtitle }}</p>
+  </div>
+</template>
