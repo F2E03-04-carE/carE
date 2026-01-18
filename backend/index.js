6ed8b493d42691b683a38ead21ad6b4eb98a5b03
chickundersea <chickundersea@gmail.com>
2026-01-19 03:43:48 +0800

    新增：暫時用的 /debug/ip API呼叫公網 IP 查詢服務用於提交申請書

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import taxIdRoutes from './routes/taxId.js';
+import debugRoutes from './routes/debug.js';
 import 'dotenv/config'
 
 
 const app = express()
 
-// ====== 中介軟體 (Middleware) ======
 app.use(cors())
 app.use(express.json())
-app.use('/api', taxIdRoutes);
+
+
 
 // ====== 路由 (Routes) ======
 // TODO: 之後引入路由
@@ -28,3 +29,6 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+app.use('/api', taxIdRoutes);
+app.use('/debug', debugRoutes);
