d9f9070f4801c8a0ebf1d6a80fa86b222dfba010
chickundersea <chickundersea@gmail.com>
2026-01-19 14:57:53 +0800

    修改：刪除不必要的檔案

--- backend/src/controllers/debugController.js
+++
@@ -1,9 +0,0 @@
-export const getServerIp = async (req, res) => {
-  try {
-    const r = await fetch('https://api.ipify.org?format=json');
-    const data = await r.json();
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch IP' });
-  }
-};
