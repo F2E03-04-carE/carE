b82be1d093052f90c0a08faeabeedfd5a3d8be4a
hoooom6600 <hoooom6600@gmail.com>
2026-01-19 10:19:21 +0800

    新增: 維修廠後台頁面骨架

---
+++ frontend/src/views/ShopAdmin/layout/ShopAdminLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AsideMenu from './AsideMenu.vue';
+</script>
+
+<template>
+  <div class="min-h-screen text-[#4a4a43] relative">
+    <div class="flex min-h-screen">
+      <AsideMenu />
+
+      <main class="flex-1 ml-[260px] px-8 py-8 bg-white">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
