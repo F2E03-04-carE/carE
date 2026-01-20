e13eff8aa7bb8d19c0b28e383fa64105bf971dc5
chickundersea <chickundersea@gmail.com>
2026-01-21 00:18:15 +0800

    新增：加入行政區套件篩選參數與搜尋路由導向

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,15 +3,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/join-garage',
-      name: 'JoinGarage',
-      component: () => import('@/views/Garage/JoinGarage.vue'),
-      meta: {
-        title: '刊登維修廠 - carE',
-      },
+  {
+    path: '/join-garage',
+    name: 'JoinGarage',
+    component: () => import('@/views/Garage/JoinGarage.vue'),
+    meta: {
+      title: '刊登維修廠 - carE',
     },
-  ],
+  },
+  {
+    path: '/search',
+    name: 'SearchResults',
+    component: () => import('@/views/Search/SearchResults.vue'),
+    meta: {
+      title: '搜尋結果 - carE',
+    },
+  },
+]
 })
 
 export default router
