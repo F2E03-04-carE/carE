2eaf8b5a3de738b1df9533ae0242f5951173775f
choco-luo <chocoyuru0081@gmail.com>
2026-01-20 14:06:48 +0800

    修改: 推送hotfix前端部署更新錯誤原因，路徑名稱錯誤

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HeaderNavbar from '@/components/Layout/HeaderNavbar.vue';
+import HeaderNavbar from '@/components/layout/HeaderNavbar.vue';
 import Home from '@/views/Home/Home.vue';
-import Footer from '@/components/Layout/Footer.vue';
+import Footer from '@/components/layout/Footer.vue';
 import SearchResults from '@/views/Search/SearchResults.vue';
 import DetailView from '@/components/Search/DetailView.vue';
 import ServiceSearchFlow from '@/components/service-search/ServiceSearchFlow.vue';
