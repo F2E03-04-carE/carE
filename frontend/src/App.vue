6a607e117036ec5635427c533488f1151a18f5b2
choco-luo <chocoyuru0081@gmail.com>
2026-01-20 11:56:50 +0800

    修改: 修正App.vue路徑問題

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import HeaderNavbar from './components/Layout/HeaderNavbar.vue';
-import Home from './views/Home/Home.vue';
-import Footer from './components/Layout/Footer.vue';
-import SearchResults from './views/Search/SearchResults.vue';
+import HeaderNavbar from '@/components/Layout/HeaderNavbar.vue';
+import Home from '@/views/Home/Home.vue';
+import Footer from '@/components/Layout/Footer.vue';
+import SearchResults from '@/views/Search/SearchResults.vue';
 import DetailView from '@/components/Search/DetailView.vue';
-import ServiceSearchFlow from './components/service-search/ServiceSearchFlow.vue';
-import VehicleManagement from './views/User/VehicleManagement.vue';
-import JoinGarage from './views/Garage/JoinGarage.vue';
-import GarageOnboarding from './views/Garage/GarageOnboarding.vue';
+import ServiceSearchFlow from '@/components/service-search/ServiceSearchFlow.vue';
+import VehicleManagement from '@/views/User/VehicleManagement.vue';
+import JoinGarage from '@/views/Garage/JoinGarage.vue';
+import GarageOnboarding from '@/views/Garage/GarageOnboarding.vue';
 
 </script>
 
